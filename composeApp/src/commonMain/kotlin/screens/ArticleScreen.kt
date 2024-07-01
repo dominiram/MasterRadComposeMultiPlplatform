b581adc5b69a4fdf529b3c77597d04b73a761b28
dominiram <40073859+dominiram@users.noreply.github.com>
2024-07-02 01:23:06 +0200

    removed unused imports

--- composeApp/src/commonMain/kotlin/screens/ArticleScreen.kt
+++ composeApp/src/commonMain/kotlin/screens/ArticleScreen.kt
@@ -13,9 +13,6 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.automirrored.filled.ArrowForward
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
